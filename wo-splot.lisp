d41c1001442cdd3582cf65e217a12aebbbfa16bf
Willem Rein Oudshoorn <woudshoo+github@xs4all.nl>
2012-09-14 15:01:59 +0200

    . Changes which I do not remember.
    Signed-off-by: Willem Rein Oudshoorn <woudshoo+github@xs4all.nl>

--- wo-splot.lisp
+++ wo-splot.lisp
@@ -20,19 +20,38 @@
    (min-time :accessor min-time)
    (max-time :accessor max-time)))
 
-(defmethod move)
+;(defmethod move)
 
-(defclass segment ()
+(defclass event ()
+  ((color :accessor color :initarg :color)
+   (ident :accessor ident :initarg :ident)))
+
+
+;;; SEGMENT CLASS
+(defclass segment (event)
   ((start :accessor start :initarg :start)
-   (stop :accessor stop :initarg :stop)
-   (color :accessor color :initarg :color)))
+   (stop :accessor stop :initarg :stop)))'
 
+;;; MOMENT CLASS
+(defclass moment (event)
+  ((moment :accessor moment :initarg :moment)))
+
+(defmethod start ((moment moment))
+  (moment moment))
+
+(defmethod stop ((moment moment))
+  (moment moment))
+;;;;;;;;;;;;;;;;;;;;;
+
+;;; CONVERSION METHODS
 (defmethod x-point ((value integer) (area draw-area))
   (with-slots (canvas-width min-time max-time) area
     (* canvas-width (/ (- value min-time) (- max-time min-time)))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defmethod set-stroke-color ((segment segment))
-  (apply #'set-rgb-stroke (color segment)))
+;;; DRAWING METHODS
+(defmethod set-stroke-color ((event event))
+  (apply #'set-rgb-stroke (color event)))
 
 (defmethod draw-at-height ((segment segment) (height number) (area draw-area))
   (move-to (x-point (start segment) area) height)
@@ -41,10 +60,13 @@
   (stroke))
 
 
-(defun make-test-data ()
-  (list 
-   (make-instance 'segment :start 10 :stop 20 :color '(1.0 0.0 0.0))
-   (make-instance 'segment :start 18 :stop 40 :color '(0.0 1.0 0.0))))
+(defmethod draw-at-height ((moment moment) (height number) (area draw-area))
+  (centered-circle-path (x-point (moment moment) area) height 1.0)
+  (set-stroke-color moment)
+  (stroke))
+
+
+;;; DRAW AREA METHODS
 
 (defun configure-area-from-data (area data)
   (loop :for segment :in data
@@ -53,7 +75,18 @@
      :finally (progn (setf (min-time area) min-start) (setf (max-time area) max-end))))
 
 
-(defun draw-data (data file-name &key (width 2000) (height (length data)))
+(defun ident-height-map (data)
+  (loop :with result = (make-hash-table :test 'equalp)
+     :with max = -1
+     :for element :in data
+     :finally (return result)
+     :do
+     (unless (gethash (ident element) result)
+       (setf (gethash (ident element) result) (incf max)))))
+
+(defun draw-data (data file-name &key 
+		  (width 2000)
+		  (height (hash-table-count (ident-height-map data))))
   "Draws the data to the png with file-name"
   (with-canvas (:width width :height height)
     (set-rgb-fill 1.0 1.0 1.0)
@@ -61,64 +94,82 @@
     (fill-path)
     (let ((area (make-instance 'draw-area :canvas-height height :canvas-width width)))
       (configure-area-from-data area data)
-      (loop :for element :in data 
-	 :for height :from 0.5 :by 1
+      (loop 
+	 :with height-map = (ident-height-map data)
+	 :for element :in data 
+	 :for height = (gethash (ident element) height-map)
 	 :do
-	 (draw-at-height element height area)))
+	 (draw-at-height element (+ 0.5 height) area)))
     (save-png file-name)))
        
 
+;;; PARSING
 
 (defparameter *start-expression* "going to sleep$")
 (defparameter *stop-expression* "waking up$")
 (defparameter *time-expression* ".{15}")
+(defparameter *moment-expression* nil)
+(defparameter *ident-expression* nil)
 
 (defun parse-file (file-name config-name)
-  (let (*start-expression* *stop-expression* *time-expression*)
+  "Parses the log in `file-name' by the patterns specified by the `config-name'.
+Return  a list of event entries."
+  (let (*start-expression* *stop-expression* *time-expression* *moment-expression* *ident-expression*)
     (load config-name)
     (with-open-file (s file-name :external-format :utf-8)
-      (loop :for line = (read-line s nil nil)
-	 :while line
+      (loop 
+	 :with start-expr = (and *start-expression* (cl-ppcre:create-scanner *start-expression*))
+	 :with stop-expr = (and *stop-expression* (cl-ppcre:create-scanner *stop-expression*))
+	 :with time-expr = (and *time-expression* (cl-ppcre:create-scanner *time-expression*))
+	 :with moment-expr = (and *moment-expression* (cl-ppcre:create-scanner *moment-expression*))
+	 :with ident-expr = (and *ident-expression* (cl-ppcre:create-scanner *ident-expression*))
 	 :with result = (list)
+	 :for line = (read-line s nil nil) :while line
 	 :finally (return result)
+	 :for element = (list)
 	 :do
-	 (when (cl-ppcre:scan *start-expression* line)
-	   (let ((time-string (cl-ppcre:scan-to-strings *time-expression* line)))
-	     (push (list :start (net.telent.date:parse-time time-string)) result)))
-	 (when (cl-ppcre:scan *stop-expression* line)
-	   (let ((time-string (cl-ppcre:scan-to-strings *time-expression* line)))
-	     (push (list :stop (net.telent.date:parse-time time-string)) result)))))))
+	 (when (and start-expr (cl-ppcre:scan start-expr line))
+	   (push :start element))
+	 (when (and stop-expr (cl-ppcre:scan stop-expr line))
+	   (push :stop element))
+	 (when (and moment-expr (cl-ppcre:scan moment-expr line))
+	   (push :moment element))
+	 (when element
+	   (let ((time-string (cl-ppcre:scan-to-strings time-expr line)))
+	     (push (net.telent.date:parse-time time-string) element))
+	   (push :ident element)
+	   (push (and ident-expr (cl-ppcre:scan-to-strings ident-expr line)) element)
+	   (push (nreverse element) result))))))
 
+(defun visualize-log (log-file-name config-file-name png-file-name)
+  (draw-data (convert-simple-data (parse-file log-file-name config-file-name)) png-file-name))
 
-(defun parse-sleep-file (file-name)
-  (with-open-file (s file-name :external-format :utf-8)
-    (loop :for line = (read-line s nil nil)
-       :while line
-       :with result = (list)
-       :finally (return result)
-       :do
-       (when (cl-ppcre:scan *start-expression* line)
-	 (let ((time-string (cl-ppcre:scan-to-strings *time-expression* line)))
-	   (push (list :start (net.telent.date:parse-time time-string)) result)))
-       (when (cl-ppcre:scan *stop-expression* line)
-	 (let ((time-string (cl-ppcre:scan-to-strings *time-expression* line)))
-	   (push (list :stop (net.telent.date:parse-time time-string)) result)))
-       )))
-
+;;; CONVERSION METHODS
 
 (defun convert-simple-data (list)
   (let ((slist (sort (copy-seq list) #'< :key #'second)))
-    (loop :for (type time) :in slist
-       :with start = nil
-       :with stop = nil
+    (loop :for (type time . rest) :in slist
+       :for ident = (getf rest :ident "<NONE>")
+       :with start = (make-hash-table :test #'equal)
        :with result = (list)
        :finally (return result)
        :do
        (case type
 	 (:start 
-	  (when stop
-	    (push (make-instance 'segment :start stop :stop time :color (list 1.0 0.0 0.0)) result))
-	  (setf start time))
-	 (:stop (when start
-		  (push (make-instance 'segment :start start :stop time :color (list 0.0 0.0 1.0)) result))
-		(setf stop time))))))
+	  (setf (gethash ident start)  time))
+	 (:stop 
+	  (multiple-value-bind (start found) (gethash ident start)
+	    (when found
+	      (push (make-instance 'segment 
+				   :ident ident
+				   :start start 
+				   :stop time 
+				   :color (list 0.0 0.0 1.0)) 
+		    result))))
+	 (:moment
+	  (push (make-instance 'moment 
+			       :ident ident
+			       :moment time 
+			       :color (list 1.0 0.0 0.0))
+		result))))))
+
